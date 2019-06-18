fd9cd0ecb51a8f776c0b8d249f0e34367a93e887
songguo6 <zhanglisong25@163.com>
2019-06-18 21:21:59 +0800

    table

--- src/pages/CandyPage.js
+++ src/pages/CandyPage.js
@@ -40,9 +40,21 @@ const tag = (record) => {
   return '';
 }
 
+const status = (record) => {
+  const now = new Date().getTime();
+  if(now < record.start_time){
+    return 0;
+  }else if(now >= record.start_time && now < record.end_time){
+    return 1;
+  }else if(now >= record.end_time){
+    return 2;
+  }
+  return 0;  
+}
+
 const columns = [
-  { title: '状态', dataIndex: 'id', key: 'id', render: (text, record) => tag(record)},
-  { title: '类型', dataIndex: 'type', key: 'type', width: 105, render: text => typeToText(text)},
+  { title: '状态', dataIndex: 'id', key: 'id', render: (text, record) => tag(record), sorter: (a, b) => status(a) - status(b), defaultSortOrder: 'ascend'},
+  { title: '类型', dataIndex: 'type', key: 'type', width: 105, render: text => typeToText(text), sorter: (a, b) => a.type - b.type},
   { title: '标题', dataIndex: 'title', key: 'titile', render: (text, record) => <a href={record.link} target='_blank' rel="noopener noreferrer">{text}</a>},
   { title: '详情', dataIndex: 'info', key: 'info', width: 750 },
   { title: '价值', dataIndex: 'reward', key: 'reward'},
