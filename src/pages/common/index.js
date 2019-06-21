fdf5c6db5e2c0a91e6cdbd1702950c503a4f574a
songguo6 <zhanglisong25@163.com>
2019-06-21 10:39:00 +0800

    code

--- src/pages/common/index.js
+++ src/pages/common/index.js
@@ -18,3 +18,38 @@ export const createItem = (item, index, width = '20%') => {
     </Card.Grid>
   )
 }
+
+export const createMainCard = (title, data, extra = '') => {
+  return (
+    <Card
+      style={{ marginBottom: 24 }}
+      bodyStyle={{ padding: 0 }}
+      bordered={false}
+      title={title}
+      extra={extra}
+    >
+      {
+        data.map((item, index) => (
+          createItem(item, index)
+        ))
+      }
+    </Card>
+  )
+}
+
+export const createSideCard = (title, data) => {
+  return (
+    <Card
+    style={{ marginBottom: 24 }}
+    title={title}
+    bordered={false}
+    bodyStyle={{ padding: 0 }}
+    >
+      {
+        data.map((item, index) => (
+          createItem(item, index, '33.33%')
+        ))
+      }
+    </Card>
+  )
+}
--- src/pages/dashboard/HomePage.js
+++ src/pages/dashboard/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Row, Col, Card } from 'antd';
-import { createItem } from '../common';
+import { Row, Col } from 'antd';
+import { createSideCard, createMainCard } from '../common';
 import { exchanges, dxchanges, links, infos, tools } from '../../data/home';
 
 class HomePage extends Component {
@@ -9,73 +9,20 @@ class HomePage extends Component {
     return (
       <Row gutter={24}>
         <Col xl={15} lg={24} md={24} sm={24} xs={24}>
-          <Card
-            style={{ marginBottom: 24 }}
-            bodyStyle={{ padding: 0 }}
-            bordered={false}
-            title='交易所公告'
-            extra={
+          {
+            createMainCard('交易所公告', exchanges, 
               <span>
                 <a href="https://www.feixiaohao.com/exchange/notice" target="_blank" rel="noopener noreferrer">非小号&nbsp;|&nbsp;</a>
                 <a href="https://www.mytoken.io/media/notice" target="_blank" rel="noopener noreferrer">MyToken</a>
-              </span>
-            }
-          >
-            {
-              exchanges.map((item, index) => (
-                createItem(item, index)
-              ))
-            }
-          </Card>
-
-          <Card
-            bodyStyle={{ padding: 0 }}
-            bordered={false}
-            title='去中心化交易所'
-          >
-            {
-              dxchanges.map((item, index) => (
-                createItem(item, index)
-              ))
-            }
-          </Card>
+              </span>)
+          }
+          {createMainCard('去中心化交易所', dxchanges)}
         </Col>
 
         <Col xl={9} lg={24} md={24} sm={24} xs={24}>
-          <Card
-            style={{ marginBottom: 24 }}
-            title='行情'
-            bordered={false}
-            bodyStyle={{ padding: 0 }}
-          >
-            {
-              links.map((item, index) => (
-                createItem(item, index, '33.33%')
-              ))
-            }
-          </Card>
-          <Card
-            style={{ marginBottom: 24 }}
-            bordered={false}
-            title='资讯'
-          >
-            {
-              infos.map((item, index) => (
-                createItem(item, index, '33.33%')
-              ))
-            }
-          </Card>
-          <Card
-            bodyStyle={{ marginBottom: 24 }}
-            bordered={false}
-            title='工具'
-          >
-            {
-              tools.map((item, index) => (
-                createItem(item, index, '33.33%')
-              ))
-            }
-          </Card>
+          {createSideCard('行情', links)}
+          {createSideCard('资讯', infos)}
+          {createSideCard('工具', tools)}
         </Col>
       </Row>
     )
