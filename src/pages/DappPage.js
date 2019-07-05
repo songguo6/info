121fce4c2da17fb4efa07a0d8d4169bdc1f88f12
songguo6 <zhanglisong25@163.com>
2019-07-05 23:26:33 +0800

    info

--- src/pages/DappPage.js
+++ src/pages/DappPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-import { createMainCard, createSideCard } from './common';
+import { createMainCard } from './common';
 import { navigations, dapps } from '../data/dapp';
 
 const DappPage = () => (
@@ -11,9 +11,7 @@ const DappPage = () => (
     </Col>
 
     <Col xl={9} lg={24} md={24} sm={24} xs={24}>
-      {createSideCard('', [])}
-      {createSideCard('', [])}
-      {createSideCard('', [])}
+
     </Col>
   </Row>
 )
