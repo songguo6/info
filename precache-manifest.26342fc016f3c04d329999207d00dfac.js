90cfd7e83fb83369c260cd3251f5cb295dea04a8
songguo6 <zhanglisong25@163.com>
2019-07-07 16:00:34 +0800

    Updates

---
+++ precache-manifest.26342fc016f3c04d329999207d00dfac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a8c83144d6363f8ace8453f4d7f2b7",
+    "url": "/xxz/index.html"
+  },
+  {
+    "revision": "62033d3f278d852779d0",
+    "url": "/xxz/static/css/2.039d199d.chunk.css"
+  },
+  {
+    "revision": "236aca40930504730573",
+    "url": "/xxz/static/css/main.44b7e1fe.chunk.css"
+  },
+  {
+    "revision": "62033d3f278d852779d0",
+    "url": "/xxz/static/js/2.ef61b5f0.chunk.js"
+  },
+  {
+    "revision": "236aca40930504730573",
+    "url": "/xxz/static/js/main.7f5b9828.chunk.js"
+  },
+  {
+    "revision": "e4c8be4d7a6f5f449f29",
+    "url": "/xxz/static/js/runtime~main.773c1a6c.js"
+  }
+]);
