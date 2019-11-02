d99929ff08d33f2ad3f0fb5136c3229e5c658624
songguo6 <zhanglisong25@163.com>
2019-11-02 17:44:50 +0800

    info

--- src/pages/developer/EosDevPage.js
+++ src/pages/developer/EosDevPage.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-import { createMainCard, createSideCard } from '../common';
-import { 
-  forums, eosdocs, eosblogs, eostools, ipfs, polkadot, apis, ides, githubs, 
-  sites, resources, tools, 
-} from '../../data/developer';
+
+const i = (title, info) => (
+  <div>
+    <h4>{title}</h4>
+    <h6>{info}</h6>
+  </div>
+)
 
 const EosDevPage = () => (
   <Row gutter={24}>
     <Col xl={15} lg={24} md={24} sm={24} xs={24}>
-      {createMainCard('技术社区', forums)}
-      {createMainCard('EOS开发文档', eosdocs)}
-      {createMainCard('EOS技术博客', eosblogs)}
-      {createMainCard('EOS开发工具', eostools)}
-      {createMainCard('IPFS开发文档 & 技术博客 & 开发工具', ipfs)}
-      {createMainCard('波卡开发文档 & 技术博客 & 开发工具', polkadot)}
-      {createMainCard('API & 插件', apis)}
-      {createMainCard('IDE & 代码编辑器', ides)}
-      {createMainCard('Github', githubs)}
+      {i('解锁钱包', 'cleos wallet unlock')}
+      {i('导入私钥到钱包', 'cleos wallet import')}
+      {i('购买RAM', "cleos -u https://api.eosnewyork.io system buyram gamesforboys gamesforboys '18.0000 EOS'")}
+      {i('编译合约', 'eosio-cpp -o gamesforboys.wasm gamesforboys.cpp --abigen')}
+      {i('部署合约', 'cleos -u https://api.eosnewyork.io set contract gamesforboys gamesforboys -p gamesforboys@active')}
     </Col>
 
     <Col xl={9} lg={24} md={24} sm={24} xs={24}>
-      {createSideCard('通用学习网站', sites)}
-      {createSideCard('通用学习资源', resources)}
-      {createSideCard('通用工具', tools)}
+
     </Col>
   </Row>
 )
