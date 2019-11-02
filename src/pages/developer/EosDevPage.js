c26aff677b95bfc20215bfe64ac44c43c6e5454e
songguo6 <zhanglisong25@163.com>
2019-11-02 22:36:43 +0800

    info

--- src/pages/developer/EosDevPage.js
+++ src/pages/developer/EosDevPage.js
@@ -4,13 +4,14 @@ import { List } from 'antd';
 const data = [
   {'t':'解锁钱包', 'i':'cleos wallet unlock'},
   {'t':'导入私钥到钱包', 'i':'cleos wallet import'},
-  {'t':'购买RAM', 'i':'cleos -u https://api.eosnewyork.io system buyram gamesforboys gamesforboys "18.0000 EOS"'},
   {'t':'编译合约', 'i':'eosio-cpp -o gamesforboys.wasm gamesforboys.cpp --abigen'},
+  {'t':'购买RAM', 'i':'cleos -u https://api.eosnewyork.io system buyram gamesforboys gamesforboys "18.0000 EOS"'},
   {'t':'部署合约', 'i':'cleos -u https://api.eosnewyork.io set contract gamesforboys gamesforboys -p gamesforboys@active'},
   {'t':'调用合约', 'i':'cleos -u https://api.eosnewyork.io push action gamesforboys init \'[]\' -p gamesforboys'},
   {'t':'设置eosio.code权限', 'i':'cleos -u https://api.eosnewyork.io set account permission gamesforboys active \'{"threshold": 1,"keys": [{"key": "EOS5DBwXHwmq9igQe39w6LRDHZmb7EjrALXAnATvxoeYLpHxWgPS7","weight": 1}],"accounts": [{"permission":{"actor":"gamesforboys","permission":"eosio.code"},"weight":1}]}\' owner -p gamesforboys'},
   {'t':'充值EOS到Rexfund', 'i':'cleos -u https://api.eosnewyork.io system rex deposit gamesforboys "2.0000 EOS" -p gamesforboys'},
   {'t':'租用CPU', 'i':'cleos -u https://api.eosnewyork.io system rex rentcpu gamesforboys gamesforboys "2.0000 EOS" "0.0000 EOS"'},
+  {'t':'查询数据表', 'i':'cleos -u https://api.eosnewyork.io get table gamesforboys gamesforboys global'},
 ];
 
 const EosDevPage = () => (
