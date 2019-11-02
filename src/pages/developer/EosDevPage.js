9828de44ba879ce84c5b5452b0aa0a44a0ff8b97
songguo6 <zhanglisong25@163.com>
2019-11-02 19:05:11 +0800

    info

--- src/pages/developer/EosDevPage.js
+++ src/pages/developer/EosDevPage.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { List } from 'antd';
 
-const i = (title, info) => (
-  <div>
-    <h4>{title}</h4>
-    <h6>{info}</h6>
-  </div>
-)
+const data = [
+  {'title':'解锁钱包', 'info':'cleos wallet unlock'},
+  {'title':'导入私钥到钱包', 'info':'cleos wallet import'},
+  {'title':'购买RAM', 'info':'cleos -u https://api.eosnewyork.io system buyram gamesforboys gamesforboys "18.0000 EOS"'},
+  {'title':'编译合约', 'info':'eosio-cpp -o gamesforboys.wasm gamesforboys.cpp --abigen'},
+  {'title':'部署合约', 'info':'cleos -u https://api.eosnewyork.io set contract gamesforboys gamesforboys -p gamesforboys@active'},
+];
 
 const EosDevPage = () => (
-  <Row gutter={24}>
-    <Col xl={15} lg={24} md={24} sm={24} xs={24}>
-      {i('解锁钱包', 'cleos wallet unlock')}
-      {i('导入私钥到钱包', 'cleos wallet import')}
-      {i('购买RAM', "cleos -u https://api.eosnewyork.io system buyram gamesforboys gamesforboys '18.0000 EOS'")}
-      {i('编译合约', 'eosio-cpp -o gamesforboys.wasm gamesforboys.cpp --abigen')}
-      {i('部署合约', 'cleos -u https://api.eosnewyork.io set contract gamesforboys gamesforboys -p gamesforboys@active')}
-    </Col>
-
-    <Col xl={9} lg={24} md={24} sm={24} xs={24}>
-
-    </Col>
-  </Row>
+  <List
+    itemLayout="horizontal"
+    dataSource={data}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          title={<a href="https://bihu.com/people/14150">{item.title}</a>}
+          description={item.info}
+        />
+      </List.Item>
+    )}
+  />
 )
 
 export default EosDevPage;
