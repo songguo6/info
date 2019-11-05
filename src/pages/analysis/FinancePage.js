af240f676eade845f3a7e5572c93c14be57d0792
songguo6 <zhanglisong25@163.com>
2019-11-05 11:48:03 +0800

    info

--- src/pages/analysis/FinancePage.js
+++ src/pages/analysis/FinancePage.js
@@ -15,12 +15,6 @@ const FinancePage = () => (
         <TradingViewWidget symbol='SPCFD:SPX' locale='zh_CN' width='100%' interval='M' theme={Themes.DARK}/>    
       </Col>
     </Row>
-    <Row gutter={24}>
-      <Col xl={12} lg={24} md={24} sm={24} xs={24}>
-        <h1 style={{textAlign:'center'}}>比特币/美元</h1>
-        <TradingViewWidget symbol='BITFINEX:BTCUSD' locale='zh_CN' width='100%' interval='M' theme={Themes.DARK}/>    
-      </Col>
-    </Row>
   </Fragment>
 )
 
