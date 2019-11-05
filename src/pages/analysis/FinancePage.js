38ab8067606de0b7d3baefea06bfb17fa39b9105
songguo6 <zhanglisong25@163.com>
2019-11-05 11:30:32 +0800

    info

--- src/pages/analysis/FinancePage.js
+++ src/pages/analysis/FinancePage.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Row, Col } from 'antd';
 import TradingViewWidget, { Themes } from 'react-tradingview-widget';
 
 const FinancePage = () => (
-  <Col>
+  <Fragment>
     <Row gutter={24}>
       <Col xl={12} lg={24} md={24} sm={24} xs={24}>
         <h1 style={{textAlign:'center'}}>道琼斯工业平均指数</h1>
@@ -21,7 +21,7 @@ const FinancePage = () => (
         <TradingViewWidget symbol='BITFINEX:BTCUSD' locale='zh_CN' width='100%' interval='M' theme={Themes.DARK}/>    
       </Col>
     </Row>
-  </Col>
+  </Fragment>
 )
 
 export default FinancePage;
