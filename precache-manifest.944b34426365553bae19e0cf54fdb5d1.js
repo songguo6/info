c1f2253063573d779e5639b9a775ad6193a090a3
songguo6 <zhanglisong25@163.com>
2020-09-13 10:56:48 +0800

    Updates

--- precache-manifest.944b34426365553bae19e0cf54fdb5d1.js
+++ precache-manifest.944b34426365553bae19e0cf54fdb5d1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d7a403e4e2b79a1d73ed4ac9597a66ab",
+    "revision": "5eeab9ad0dbb771081b2f5a547751ea9",
     "url": "/info/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/info/static/css/2.365b9c9b.chunk.css"
   },
   {
-    "revision": "60f88f9b7cae0b2195db",
+    "revision": "8eae16d920080d69065a",
     "url": "/info/static/css/main.2e1decac.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/info/static/js/2.81e52a60.chunk.js"
   },
   {
-    "revision": "60f88f9b7cae0b2195db",
-    "url": "/info/static/js/main.b0a6a886.chunk.js"
+    "revision": "8eae16d920080d69065a",
+    "url": "/info/static/js/main.a2e41eb1.chunk.js"
   },
   {
     "revision": "1d690396bbd26ac904b5",
